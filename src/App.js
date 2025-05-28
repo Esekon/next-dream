2bb883126a310df2295fc3db881ff596f36c1a4f
Esekon <egucollins@gmail.com>
2025-05-28 11:52:01 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-
-import Navbar from './Navbar/Navbar';
 import Signup from './components/Signup';
 import Signin from './components/Signin';
 import Buy from './components/Buy';
@@ -12,38 +10,29 @@ import Mainpage from './components/Mainpage';
 import MainLayout from './layouts/Mainlayout';
 import Welcomepage from './pages/Welcomepage';
 
+
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
+    <div className="App">
+    
 
-        <header className="App-header">
-          <h1>
-            <span className="imgspan">
-              {/* Provide the actual image path below */}
-              <img src="images/basket.png" alt="Basket" className="basketimage" />
-            </span>
-            NEXT <b className="text-danger">DREAM</b> - Buy your drinks online
-          </h1>
-        </header>
 
-        <br />
+      <Routes>
+        <Route path="/" element={<Welcomepage />} />
 
-        <Routes>
-          <Route path="/" element={<Welcomepage />} />
+        {/* Wrap routes with MainLayout if needed */}
+        <Route element={<MainLayout />}>
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/Signin" element={<Signin />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Buy" element={<Buy />} />
+          <Route path="/Mainpage" element={<Mainpage />} />
+        </Route>
+       
+      </Routes>
+    
 
-          {/* Wrap routes with MainLayout if needed */}
-          <Route element={<MainLayout />}>
-            <Route path="/Signup" element={<Signup />} />
-            <Route path="/Signin" element={<Signin />} />
-            <Route path="/Home" element={<Home />} />
-            <Route path="/Buy" element={<Buy />} />
-            <Route path="/Mainpage" element={<Mainpage />} />
-          </Route>
-        </Routes>
-      </div>
-    </Router>
+    </div>
   );
 }
 
