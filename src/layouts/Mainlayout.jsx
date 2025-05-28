64b760ba8fb66ffa4dbab6bd91cf631c0de86282
Esekon <egucollins@gmail.com>
2025-05-28 11:53:18 +0300

    Update Mainlayout.jsx

--- src/layouts/Mainlayout.jsx
+++ src/layouts/Mainlayout.jsx
@@ -1,6 +1,9 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../components/Footer';
+import Personals from '../components/Personals'
+import Marquee from '../components/Marquee'
+import AboutUs from '../components/Aboutus';
 
 const MainLayout = () => {
   return (
@@ -9,6 +12,9 @@ const MainLayout = () => {
       <main style={{ minHeight: '80vh' }}>
         <Outlet />
       </main>
+      <AboutUs/>
+      <Marquee/>
+      <Personals/>
       <Footer />
     </>
   );
